42f5eabaca813a5378a20743697b97b13d43f676
Amey Pankanti <apankanti@paktolus.com>
2024-02-26 18:34:37 +0530

    comment added

--- src/common/auth/jwt.module.ts
+++ src/common/auth/jwt.module.ts
@@ -9,8 +9,8 @@ import { JwtModule } from '@nestjs/jwt';
       useFactory: async (configService: ConfigService) => {
         const secret = configService.get<string>('jwt.secret');
         const expiresIn = configService.get<string>('jwt.expireLimit');
-        console.log('JWT Secret in jwt module:', secret);
-        console.log('JWT Expires In:', expiresIn);
+        // console.log('JWT Secret in jwt module:', secret);
+        // console.log('JWT Expires In:', expiresIn);
         if (!secret) {
           throw new Error('JWT secret is not defined');
         }
